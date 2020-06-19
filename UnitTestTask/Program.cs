41124c9e8849ad6ae95f3908f7b73d56abcaf979
kaska-ziolkowska <herbmasta@gmail.com>
2020-06-19 22:08:59 +0100

    Add project files.

---
+++ UnitTestTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnitTestTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
